53aa508794aa209051b9b3be0fa7ef67969160bc
pliniogim <pliniogimenez@gmail.com>
2023-01-17 20:16:40 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart
    #	lib/screens/product_detail_screen.dart
    #	lib/screens/products_overview.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,50 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  const ProductItem(this.id, this.title, this.imageUrl, {Key? key})
+  const ProductItem({Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: GridTile(
-        //header: Text(title),
-        footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-              Icons.favorite,
-              color: Theme.of(context).colorScheme.secondary,
+    //first way of using provider
+    //final product =  Provider.of<Product>(context);
+
+    // another way of using provider
+    //with the use of consumer
+    //only in the area that needs to be updated
+    return Consumer<Product>(
+      builder: (ctx, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: GridTile(
+          //header: Text(title),
+          footer: GridTileBar(
+            leading: IconButton(
+              icon: Icon(product.isFavorite ?
+                Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              onPressed: (){
+                product.toggleFavoriteStatus();
+              },
             ),
-            onPressed: null,
-          ),
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-            style: const TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.black87,
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).colorScheme.secondary,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
             ),
-            onPressed: null,
+            backgroundColor: Colors.black87,
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              onPressed: null,
+            ),
           ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id,
-            );
-          },
-          child: Image.network(
-            imageUrl,
-            fit: BoxFit.cover,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, //arguments: id,
+              );
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
